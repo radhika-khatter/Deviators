ae335c862a14421bcf84697afa8dc9d61df5a06a
Pulkit <kpulkit15234@gmail.com>
2024-11-10 21:01:56 +0530

    feat: footer edit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,19 +6,23 @@ export default function Footer() {
     <footer id="contact" className="mt-auto bg-[#1a103d] py-12">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex flex-col items-center space-y-6">
-          <Link href="/" className="text-2xl font-bold text-white">
-            Deviators Club
-          </Link>
+          <div className="text-center">
+            <Link href="/" className="text-2xl font-bold text-white">
+              Deviators Club
+            </Link>
+            <p className="opacity-80">estd: 2024</p>
+          </div>
 
           <div className="flex flex-wrap justify-center gap-6">
             {socials.map((social) => (
               <Link
                 key={social.name}
                 href={social.url}
-                className="text-gray-400 transition-colors hover:text-[#8b7ff9]"
+                className="flex gap-1 text-gray-400 transition-colors hover:text-[#8b7ff9]"
                 target="_blank"
                 rel="noopener noreferrer"
               >
+                <social.icon className="h-6 w-6" title={social.name} />
                 {social.name}
               </Link>
             ))}
--- src/data/socials.ts
+++ src/data/socials.ts
@@ -1,27 +1,42 @@
+import {
+  FaWhatsapp,
+  FaGithub,
+  FaInstagram,
+  FaLinkedin,
+  FaDiscord,
+  FaLink,
+} from "react-icons/fa";
+
 const socials = [
   {
     name: "Github",
     url: "https://github.com/deviatorsclub",
+    icon: FaGithub,
   },
   {
     name: "Instagram",
     url: "https://www.instagram.com/deviatorsclub",
+    icon: FaInstagram,
   },
   {
     name: "LinkedIn",
     url: "https://www.linkedin.com/company/deviators-club",
+    icon: FaLinkedin,
   },
   {
     name: "Whatsapp",
     url: "https://chat.whatsapp.com/Fq2znTYctiq3cD4Y1ebHeR",
+    icon: FaWhatsapp,
   },
   {
     name: "Discord",
     url: "https://discord.gg/n9aDPwRK6N",
+    icon: FaDiscord,
   },
   {
     name: "Linktree",
     url: "https://linktree.deviatorsdce.tech",
+    icon: FaLink,
   },
 ];
 
